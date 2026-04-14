56fcb20ca0f4f6ad2c4672de3f1325dde94cdcf6
SwaggyZ <swaggyz623@gmail.com>
2026-04-14 17:56:04 +0800

    fix(nvim): 修复 less 等文件没有 treesitter parser 时 gcc 注释报错问题

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -20,7 +20,19 @@ return {
     -- enable comment
     comment.setup {
       -- for commenting tsx, jsx, svelte, html files
-      pre_hook = ts_context_commentstring.create_pre_hook(),
+      pre_hook = function(ctx)
+        local ts_comment = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()(ctx)
+        if ts_comment then
+          return ts_comment
+        end
+
+        -- neovim 0.10+ 变动：vim.treesitter.get_parser 在没有 parser 时返回 nil 而非抛错
+        -- 导致 Comment.nvim 在尝试获取没有对应 treesitter parser 的语言的注释格式时崩溃
+        local ok, parser = pcall(vim.treesitter.get_parser, vim.api.nvim_get_current_buf())
+        if not ok or not parser then
+          return require('Comment.ft').get(vim.bo.filetype, ctx.ctype) or vim.bo.commentstring
+        end
+      end,
     }
   end,
 }
